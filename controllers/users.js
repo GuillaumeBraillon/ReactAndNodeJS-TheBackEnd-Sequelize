322b880936ce1ffc6843616be49cd14a0e4a2d0f
Guillaume Braillon <32403794+Blusy38@users.noreply.github.com>
2020-02-27 17:09:04 +0100

    Add console log

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,7 @@ module.exports = {
 
   //Show the list of all 
   list(req, res) {
+    console.log('Call list All Users');
     return UsersModel
       .findAll()
       .then(users => res.status(200).send(users))
@@ -11,6 +12,7 @@ module.exports = {
   },
 
   create(req, res) {
+    console.log('Call create One User');
     return UsersModel
       .create({
         firstName: req.body.firstName,
@@ -24,6 +26,7 @@ module.exports = {
   },
 
   retrieve(req, res) {
+    console.log('Call retrieve user n°'+req.params.userId);
     return UsersModel
       .findByPk(req.params.userId)
       .then(user => {
@@ -36,37 +39,44 @@ module.exports = {
       })
       .catch(error => res.status(400).send(error));
   },
+
   update(req, res) {
+    console.log('Call update user n°'+req.params.userId);
     return UsersModel
     .findByPk(req.params.userId)
       .then(user => {
+        //console.log(user);
         if (!user) {
+          console.log('User n°'+ req.params.userId +' Not Found');
           return res.status(404).send({
-            message: 'User Not Found',
+            message: 'User n°'+ req.params.userId +' Not Found',
           });
+          
         }
 
         return user
           .update(req.body, { fields: Object.keys(req.body) })
-          .then(updatedUser => res.status(200).send(updatedUser))
+          .then(updatedUser => res.status(200).send(updatedUser),)
           .catch(error => res.status(400).send(error));
       })
       .catch(error => res.status(400).send(error));
   },
 
   destroy(req, res) {
+    console.log('Call destroy user n°'+req.params.userId);
     return UsersModel
     .findByPk(req.params.userId)
       .then(user => {
+        console.log(user);
         if (!user) {
           return res.status(404).send({
-            message: 'User Not Found',
+            message: 'User n°'+ req.params.userId +' Not Found',
           });
         }
 
         return user
           .destroy()
-          .then(() => res.status(200).send({ message: 'User deleted successfully.' }))
+          .then(() => res.status(200).send({ message: 'User n°'+ req.params.userId +' deleted successfully.' }))
           .catch(error => res.status(400).send(error));
       })
       .catch(error => res.status(400).send(error));
